a628f8524da97bcbb573fd4c0b8bc58ba3024a0c
yassine07K <elazamiy@gmail.com>
2025-05-23 17:43:40 +0200

    Lister les magasins et lister les users

---
+++ src/main/java/com/fastmarket/fastmarket_api/repository/MagasinRepository.java
@@ -0,0 +1,7 @@
+package com.fastmarket.fastmarket_api.repository;
+
+import com.fastmarket.fastmarket_api.model.Magasin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MagasinRepository extends JpaRepository<Magasin, Long> {
+}

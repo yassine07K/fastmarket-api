5d8c1fce7d6b35a9ba38370f28678c5e0653f7f9
yassine07K <elazamiy@gmail.com>
2025-05-27 16:24:17 +0200

    magasin du gerant

--- src/main/java/com/fastmarket/fastmarket_api/controller/GerantController.java
+++ src/main/java/com/fastmarket/fastmarket_api/controller/GerantController.java
@@ -1,6 +1,9 @@
 package com.fastmarket.fastmarket_api.controller;
 
+import com.fastmarket.fastmarket_api.model.Gerant;
+import com.fastmarket.fastmarket_api.model.Magasin;
 import com.fastmarket.fastmarket_api.repository.GerantRepository;
+import com.fastmarket.fastmarket_api.repository.MagasinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 
 @RestController
@@ -18,10 +23,26 @@ public class GerantController {
     @Autowired
     private GerantRepository gerantRepository;
 
+    @Autowired
+    private MagasinRepository magasinRepository;
+
     @GetMapping("/email/{email}")
     public ResponseEntity<?> getGerantByEmail(@PathVariable String email) {
         return gerantRepository.findByEmail(email)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
+
+    @GetMapping("/{gerantId}/magasin")
+    public ResponseEntity<Magasin> getMagasinByGerant(@PathVariable Long gerantId) {
+        Optional<Gerant> gerantOpt = gerantRepository.findById(gerantId);
+        if (gerantOpt.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Optional<Magasin> magasinOpt = magasinRepository.findByGerant_Id(gerantId);
+        return magasinOpt.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }

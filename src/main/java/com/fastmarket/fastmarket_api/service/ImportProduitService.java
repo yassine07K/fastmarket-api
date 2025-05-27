4624a067eed849bd81c3c59d0b9c949f47c9748a
yassine07K <elazamiy@gmail.com>
2025-05-27 16:10:04 +0200

    import csv v1

---
+++ src/main/java/com/fastmarket/fastmarket_api/service/ImportProduitService.java
@@ -0,0 +1,27 @@
+package com.fastmarket.fastmarket_api.service;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Service
+public class ImportProduitService {
+
+    public List<CSVRecord> lireProduitsDepuisCsv(MultipartFile file) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8))) {
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+                    .withFirstRecordAsHeader()
+                    .withIgnoreHeaderCase()
+                    .withTrim());
+
+            return csvParser.getRecords();
+        }
+    }
+}
